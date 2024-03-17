77ca7021552785958de4899e0223a4a950b0d7e3
Naturel1 <moi@arnaudhubert.be>
2024-03-17 18:19:10 +0100

    4.1

---
+++ Séance 2.4 - 17-03-2024 - Gestion des fichiers (2-2)/exercices.py
@@ -0,0 +1,63 @@
+import string
+import re
+from unidecode import unidecode
+
+
+def count_letters() -> list[tuple[chr, int]]:
+    letters: dict = {}
+    with open('hamlet.txt', 'r', encoding="utf-8") as f:
+        for line in f:
+            for letter in line:
+                if letter in string.ascii_letters:
+                    if letter not in letters:
+                        letters[letter] = 1
+                    else:
+                        letters[letter] += 1
+    return sorted(list(letters.items()), key=lambda x: x[1], reverse=True)
+
+
+def count_words() -> list[tuple[str, int]]:
+    words: dict[str, int] = {}
+    with open('hamlet.txt', 'r', encoding="utf-8") as f:
+        for line in f:
+            for word in line.split(" "):
+                _word = re.sub(r"[\n?.!;:\",]", "", word)
+                if _word != "" and _word != "-":
+                    if _word not in words:
+                        words[_word] = 1
+                    else:
+                        words[_word] += 1
+        return sorted(list(words.items()), key=lambda x: x[1], reverse=True)
+
+
+def crypt_simple() -> None:
+    with open("cryptage.txt", "w", encoding="utf-8") as f1:
+        with open('hamlet.txt', 'r', encoding="utf-8") as f2:
+            for line in f2:
+                _line = unidecode(line)
+                crypted_line = ""
+                for letter in _line:
+                    pass
+                    # TODO: continuer a partir d'ici
+                    # crypted_line +=
+            print(crypted_line)
+
+
+def ex_1():
+    # a
+    print("Exercice : 1.a")
+    print(count_letters())
+    # b
+    print("Exercice : 1.b")
+    print(count_words())
+    # c
+    print("Exercice : 1.c")
+    crypt_simple()
+
+
+def main():
+    ex_1()
+
+
+if __name__ == '__main__':
+    main()
